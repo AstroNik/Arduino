68d29a51bf110f793da27ebc2aaeb042c71bbc3a
vedika <vedik@10.48.41.172>
2020-10-19 15:47:13 -0400

    Sensor data is sent only when connected to server and updated wiring diagram

--- CapstoneSensorCode.ino
+++ CapstoneSensorCode.ino
@@ -1,7 +1,10 @@
-//MAKE SURE TO LOOK OVER THE DEVICE ID, AND MAKE SURE THE ONE YOU ARE SENDING IS AN INT AND NOT A STRING
-//make sure to add the into at the top, mentioning who the code is by and all that 
-
-
+/*
+ * 
+ * Code Written By: Vedika Maheshwari
+ * Date: Monday October 19th, 2020
+ * Description: Code for the arduino device to send sensor data to the server
+ * 
+*/
 
 
 #include <ESP8266WiFi.h>
@@ -10,7 +13,8 @@
 #include <EEPROM.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
-ADC_MODE(ADC_VCC);
+//Reads the voltage of battery internally and not externally (since the battery is already connected to the board)
+//ADC_MODE(ADC_VCC);
 
 //Server Variables
 const char* host = "www.ecoders.ca";
@@ -38,16 +42,14 @@ char username[50];
 char deviceName[80];
 
 //Device ID and Memory Variables
-int deviceID;
-String MemoryVar;
-bool idInMemory = false;
+String deviceID = "987654321";
 
 //Battery Amount Variable
 int batteryAmount; 
 int voltage;
 
 
-// Required for LIGHT_SLEEP_T delay mode
+//needed to use C SDK based fuctions - needed for light sleep
 extern "C" {
   #include "user_interface.h"
 }
@@ -57,30 +59,18 @@ void setup() {
   EEPROM.begin(512);
   
   connectToWifi();
-
-  //check if device id is in memory 
-  if (checkDeviceIDInMemory() == true) {
-    Serial.println("The device id is in memory");
-    //read the device id from memory, and save it to deviceID variable
-    for (int i=0; i<9; i++){
-      MemoryVar += EEPROM.read(i); 
-    }
-    Serial.println(MemoryVar);
-  }
-  else {
-    Serial.println("New device id will be assigned to this device");
-    getDeviceID();
-    Serial.println(MemoryVar);
-  }
+  
   //Send the user email, plant name and device id to server
   sendUserCredentialsToBackend();
 }
 
 void loop() {  
   //determine what the server response is after retrieving the user email, plant name and device id
+  
+
+  //check the server response to determine if the user email exists in the database
   if (serverResponse == "HTTP/1.1 520 Origin Error") {
     Serial.println("The user email does not exist in the database");
-    //TO DO: figure out how to redirect the user to be able to successfully input their credentials again 
   }
   else {
     //send the sensor data to the server if user email is valid
@@ -89,105 +79,51 @@ void loop() {
   
   //Loop every 3 minutes
   delay(60000*3-800); 
+  
 }
-
 
 //Create a secure connection and send sensor data to the server
 void createTLSConnection() {
   // Use WiFiClientSecure class to create TLS connection
   client.setInsecure();
+  batteryLevel();
+  readSensorData();
+  JSONDocument();
+  
   Serial.print("connecting to ");
   Serial.println(host);
   if (!client.connect(host, httpsPort)) {
     Serial.println("Connection failed");
     return;
   }
-  batteryLevel();
-  readSensorData();
-  JSONDocument();
-  Serial.println("\nStarting connection to server...");
-  if (!client.connect(host, 443))
-    Serial.println("Connection failed!");
   else {
     Serial.println("Connection successful!");
-  }
-  while (client.connected()) {
-    client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n" + 
-               "Content-Type: application/json" + "\r\n" +
-               "Content-Length: " + requestBody.length() + "\r\n" +
-               "\r\n" + requestBody + "\r\n");
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("Headers received");
-      break;
+    while (client.connected()) {
+      client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" + 
+                 "Content-Type: application/json" + "\r\n" +
+                 "Content-Length: " + requestBody.length() + "\r\n" +
+                 "\r\n" + requestBody + "\r\n");
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("Headers received");
+        break;
+      }
     }
   }
   
   //After sending the post request to the server esp goes into light sleep
-  lightSleep();
+  //lightSleep();
 }
 
-//Determines if device id is in the device's memory
-bool checkDeviceIDInMemory() {
-  for (int i = 0; i < 9; i++) {
-    if (EEPROM.read(i) != 0) {
-      idInMemory = true;
-      break;
-    }
-    Serial.println("This byte is a 0");
-  }
-  return idInMemory;
-}
-
-//Creates a random new device id 
-void getDeviceID() {
-  deviceID = random(1,999999999);
-  Serial.println("");
-  Serial.println("Device ID: ");
-  Serial.println(deviceID);
-  Serial.println("");
-
-  MemoryVar = (String)deviceID;
-  
-  addDataToEEPROM();
-}
-
-//Saves each int seperately into the memory as bytes
-void addDataToEEPROM(){
-  //seperate each digit from the deviceID and save it in seperate variables
-  int n1,n2,n3,n4,n5,n6,n7,n8,n9;
-  n9 = deviceID % 10;
-  n8 = deviceID / 10 % 10;
-  n7 = deviceID / 100 % 10;
-  n6 = deviceID / 1000 % 10;
-  n5 = deviceID / 10000 % 10;
-  n4 = deviceID / 100000 % 10;
-  n3 = deviceID / 1000000 % 10;
-  n2 = deviceID / 10000000 % 10;
-  n1 = deviceID / 100000000 % 10;
-  
-  //Write these numbers to the eeprom
-  EEPROM.write(0,n1);
-  EEPROM.write(1,n2);
-  EEPROM.write(2,n3);
-  EEPROM.write(3,n4);
-  EEPROM.write(4,n5);
-  EEPROM.write(5,n6);
-  EEPROM.write(6,n7);
-  EEPROM.write(7,n8);
-  EEPROM.write(8,n9);
-
-  EEPROM.commit();
-}
-
+//
 void lightSleep() {
   Serial.println("ESP8266 going into light sleep for 15 minutes");
   WiFi.mode(WIFI_STA);
-  //wifi_set_sleep_type(LIGHT_SLEEP_T);
-  //delay(60000*15);
-  ESP.deepSleep(900e6);
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+  delay(60000*15);
+  //ESP.deepSleep(900e6);
 }
 
 //Retrieves the voltage of battery, and changes it to battery percentage amount 
@@ -204,19 +140,14 @@ void batteryLevel() {
       break;
     case 1501 ... 2250:
       batteryAmount = 75;
-    case 2251 ... 3000:
-      batteryAmount = 100;
       break;
     default: 
-      batteryAmount = 0;
+      batteryAmount = 100;
       break;
   }
 }
 
 void connectToWifi() {
-  //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
     //reset saved settings
     wifiManager.resetSettings();
     //fetches ssid and pass from eeprom and tries to connect
@@ -225,7 +156,7 @@ void connectToWifi() {
     //and goes into a blocking loop awaiting configuration
 
     //initializing the email parameter
-    WiFiManagerParameter custom_username("username", "Enter Username: ", username, 50);
+    WiFiManagerParameter custom_username("username", "Enter Email: ", username, 50);
     WiFiManagerParameter custom_deviceName("deviceName", "Enter Device Name: ", deviceName, 80);
     
     //If it restarts and router is not yet online, it keeps rebooting and retrying to connect
@@ -247,6 +178,7 @@ void connectToWifi() {
     strcpy(username, custom_username.getValue());
     strcpy(deviceName, custom_deviceName.getValue());
 }
+
 void sendUserCredentialsToBackend() {
   //Place credentials in JSON document
   saveCredentialsInJsonDocument();
@@ -259,36 +191,44 @@ void sendUserCredentialsToBackend() {
     Serial.println("Connection failed");
     return;
   }
-  
-  Serial.println("\nStarting connection to server...");
-  if (!client.connect(host, 443))
-    Serial.println("Connection failed!");
   else {
     Serial.println("Connection successful!");
-  }
   
-  while (client.connected()) {
-    client.print(String("POST ") + urlEmail + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n" + 
-               "Content-Type: application/json" + "\r\n" +
-               "Content-Length: " + requestBodyLogin.length() + "\r\n" +
-               "\r\n" + requestBodyLogin + "\r\n");
-               
-    Serial.println("request sent");
     while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
+      client.print(String("POST ") + urlEmail + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" + 
+                 "Content-Type: application/json" + "\r\n" +
+                 "Content-Length: " + requestBodyLogin.length() + "\r\n" +
+                 "\r\n" + requestBodyLogin + "\r\n");
+                 
+      Serial.println("request sent");
+  
+      while (client.connected()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          Serial.println("headers received");
+          break;
+        }
       }
+  
+      //additional quotation marks, so i have to remove them
+      serverResponse = client.readStringUntil('\n');
+      char no = '"';
+      char c;
+      for (int i=0; i<serverResponse.length();++i){
+          c = serverResponse.charAt(i);
+          if(c==no){
+              serverResponse.remove(i, 1);
+          }
+      }
+      
+      Serial.println("reply was:");
+      Serial.println("==========");
+      Serial.println(serverResponse);
+      Serial.println("==========");
+      Serial.println("closing connection");
     }
-    serverResponse = client.readStringUntil('\n');
-    Serial.println("reply was:");
-    Serial.println("==========");
-    Serial.println(serverResponse);
-    Serial.println("==========");
-    Serial.println("closing connection");
   }
   
 }
@@ -297,7 +237,7 @@ void saveCredentialsInJsonDocument() {
   JsonObject rootOne = userLoginDoc.to<JsonObject>();
   //sending data to JSON document
   rootOne["Email"] = username;
-  rootOne["DeviceId"] = MemoryVar;
+  rootOne["DeviceId"] = deviceID;
   rootOne["DeviceName"] = deviceName;
   serializeJsonPretty(userLoginDoc, requestBodyLogin);
   serializeJsonPretty(userLoginDoc, Serial);
@@ -307,7 +247,7 @@ void JSONDocument() {
   JsonObject root = doc.to<JsonObject>();
   //sending data to JSON document
   root["UID"] = serverResponse;
-  root["DeviceId"] = MemoryVar;
+  root["DeviceId"] = deviceID;
   root["Battery"] = batteryAmount;
   root["AirValue"] = AirValue;
   root["WaterValue"] = WaterValue;
